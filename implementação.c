a3aaa5be64fcb00faab79d7e32715dc294671539
KmethL <lucasmarianok@gmail.com>
2025-02-07 17:00:27 -0300

    Add files via upload

---
+++ implementação.c
@@ -0,0 +1,18 @@
+#include "cadastro.h"
+#include <stdio.h>
+
+void cadastrarJogadores(Jogador *jogadores, int *qtd) {
+    // Implementação da função
+}
+
+void listarJogadores(Jogador *jogadores, int qtd) {
+    // Implementação da função
+}
+
+void excluirJogador(Jogador *jogadores, int *qtd) {
+    // Implementação da função
+}
+
+void jogar(Jogador *jogadores, int qtd) {
+    // Implementação da função
+}
