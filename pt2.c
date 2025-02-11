be4ad6d5450dad3d2653018dfa05ccc9096b1236
KmethL <lucasmarianok@gmail.com>
2025-02-11 12:05:10 -0300

    Update cadastrocod.c

--- pt2.c
+++ pt2.c
@@ -3,34 +3,32 @@
 #include <string.h>
 #include "cadastro.h"
 
-#define MAX_JOGADORES 100
-
 void limparBuffer() {
     int c;
     while ((c = getchar()) != '\n' && c != EOF);
 }
 
 void cadastrarJogadores(Jogador *jogadores, int *qtd) {
-    if (*qtd >= MAX_JOGADORES) {
+    if (*qtd >= 50) {
         printf("Limite de jogadores atingido!\n");
         return;
     }
 
     printf("Nome do jogador: ");
     fgets(jogadores[*qtd].nome, sizeof(jogadores[*qtd].nome), stdin);
-    strtok(jogadores[*qtd].nome, "\n");
+    jogadores[*qtd].nome[strcspn(jogadores[*qtd].nome, "\n")] = '\0';
 
     printf("Nacionalidade: ");
     fgets(jogadores[*qtd].nacionalidade, sizeof(jogadores[*qtd].nacionalidade), stdin);
-    strtok(jogadores[*qtd].nacionalidade, "\n");
+    jogadores[*qtd].nacionalidade[strcspn(jogadores[*qtd].nacionalidade, "\n")] = '\0';
 
     printf("Clube: ");
     fgets(jogadores[*qtd].clube, sizeof(jogadores[*qtd].clube), stdin);
-    strtok(jogadores[*qtd].clube, "\n");
+    jogadores[*qtd].clube[strcspn(jogadores[*qtd].clube, "\n")] = '\0';
 
     printf("Posição: ");
     fgets(jogadores[*qtd].posicao, sizeof(jogadores[*qtd].posicao), stdin);
-    strtok(jogadores[*qtd].posicao, "\n");
+    jogadores[*qtd].posicao[strcspn(jogadores[*qtd].posicao, "\n")] = '\0';
 
     printf("Velocidade: ");
     scanf("%d", &jogadores[*qtd].velocidade);
@@ -45,7 +43,7 @@ void cadastrarJogadores(Jogador *jogadores, int *qtd) {
     printf("Físico: ");
     scanf("%d", &jogadores[*qtd].fisico);
 
-    limparBuffer();
+    limparBuffer(); // Evita problemas com fgets após scanf()
     (*qtd)++;
 }
 
@@ -70,10 +68,12 @@ void excluirJogador(Jogador *jogadores, int *qtd) {
     printf("Digite o número do jogador a ser removido: ");
     scanf("%d", &id);
     limparBuffer();
+    
     if (id < 1 || id > *qtd) {
         printf("Número inválido.\n");
         return;
     }
+    
     for (int i = id - 1; i < *qtd - 1; i++) {
         jogadores[i] = jogadores[i + 1];
     }
