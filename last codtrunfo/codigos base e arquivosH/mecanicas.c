e741c4fbd5004b30d6f93508c57e2cf041cd90db
KmethL <lucasmarianok@gmail.com>
2025-02-13 20:38:07 -0300

    Update mecanicas.c

--- last codtrunfo/codigos base e arquivosH/mecanicas.c
+++ last codtrunfo/codigos base e arquivosH/mecanicas.c
@@ -1,14 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <time.h>
 #include "cadastro.h"
 
 
-
-// GOATs: Cristiano Ronaldo e Messi
-Jogador CR7 = {"Cristiano Ronaldo", "Portugal", "Real Madrid", "Atacante", 92, 92, 81, 91, 33, 80};
-Jogador Messi = {"Lionel Messi", "Argentina", "Barcelona", "Atacante", 93, 88, 86, 95, 24, 62};
 
 // Lista de rivalidades (clubes e seleções)
     char *rivalidades[][2] = {
@@ -24,7 +19,9 @@ Jogador Messi = {"Lionel Messi", "Argentina", "Barcelona", "Atacante", 93, 88, 8
     int num_rivalidades = sizeof(rivalidades) / sizeof(rivalidades[0]);
 
 // Função para verificar se dois jogadores são rivais
-int saoRivais(Jogador j1, Jogador j2) {
+jogador =JOGADOR;
+
+int saoRivais(jogador j1, jogador j2) {
     for (int i = 0; i < num_rivalidades; i++) {
         if ((strcmp(j1.clube, rivalidades[i][0]) == 0 && strcmp(j2.clube, rivalidades[i][1]) == 0) ||
             (strcmp(j1.clube, rivalidades[i][1]) == 0 && strcmp(j2.clube, rivalidades[i][0]) == 0) ||
@@ -33,43 +30,5 @@ int saoRivais(Jogador j1, Jogador j2) {
             return 1;
         }
     }
-    return 0;
-}
-
-// Função principal com menu
-int main() {
-    srand(time(NULL));
-    Jogador jogadores[30];
-    int qtd = 0;
-    char opcao[10];
-    do {
-        printf("\n--- MENU ---\n");
-        printf("1. Cadastrar jogador\n");
-        printf("2. Listar jogadores\n");
-        printf("3. Excluir jogador\n");
-        printf("4. Iniciar partida\n");
-        printf("5. Sair\n");
-        printf("Escolha uma opcaoo: ");
-        fgets(opcao, sizeof(opcao), stdin);
-        switch (atoi(opcao)) {
-            case 1:
-                cadastrarJogadores(jogadores, &qtd);
-                break;
-            case 2:
-                listarJogadores(jogadores, qtd);
-                break;
-            case 3:
-                excluirJogador(jogadores, &qtd);
-                break;
-            case 4:
-                jogar(jogadores, qtd);
-                break;
-            case 5:
-                printf("Saindo...\n");
-                break;
-            default:
-                printf("Opção inválida. Tente novamente.\n");
-        }
-    } while (atoi(opcao) != 5);
     return 0;
 }
